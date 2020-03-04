eee0882a7323e40561ece2c242320b9d1172ecb5
Lokie <lokeshannem@Lokeshs-MacBook-Pro.local>
2020-03-03 21:19:12 -0500

    address footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,11 +5,15 @@ const Footer = () => {
   return (
     <footer className='footer'>
       <div className='container'>
-        <div className='content has-text-centered'>
-          <p>
-            2 St Clair Avenue West,16th Floor <br/> Toronto,ON M4V 1L5
+      <div className='content has-text-left'>
+        <p>
+          <b>Address</b>: <br/>
+            2 St Clair Avenue West,16th Floor <br/> Toronto,ON M4V 1L5 <br/>
             email: <b>contact@datatechinc.ca</b>
           </p>
+        </div>
+        <div className='content has-text-centered'>
+          
           <p>
             {config.copyright}
           </p>
